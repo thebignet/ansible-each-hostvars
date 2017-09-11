17d0e3b84e228f892437c8a3cd484d214ad86fe3
Jean Detoeuf <thebignet@gmail.com>
2017-09-11 21:47:47 +0200

    added documentation and exception handling

--- filter_plugins/group_host_vars.py
+++ filter_plugins/group_host_vars.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python
+
+from ansible import errors
+
 class FilterModule(object):
     def filters(self):
         return {
@@ -8,21 +11,39 @@ class FilterModule(object):
         }
 
     def each_hostvars(self, hostnames, hostvars):
-        return [
-            hostvars[hostname]
-            for hostname in hostnames
-        ]
+        ''' Get a list of hostvars from a list of hostnames '''
+        try:
+            return [
+                hostvars[hostname]
+                for hostname in hostnames
+            ]
+        except Exception as e:
+            raise errors.AnsibleFilterError(
+                    'each_hostvars plugin error: {0}, hostnames={1}'.format(str(e), str(hostnames))
+            )
 
     def keep_attribute(self, server_hostvars, key):
-        return [
-            hostvars[key]
-            for hostvars in server_hostvars
-        ]
+        ''' Get a list of values corresponding to a key from a list of hostvars '''
+        try:
+            return [
+                hostvars[key]
+                for hostvars in server_hostvars
+            ]
+        except Exception as e:
+            raise errors.AnsibleFilterError(
+                    'keep_attribute plugin error: {0}, key={1}'.format(str(e), str(key))
+            )
 
-    def each_format(self, server_hostvars, f):
-        return [
-            f.format_map(hostvars)
-            for hostvars in server_hostvars
-        ]
+    def each_format(self, server_hostvars, string_format):
+        ''' Get a list of formated strings using a list of hostvars '''
+        try:
+            return [
+                string_format.format_map(hostvars)
+                for hostvars in server_hostvars
+            ]
+        except Exception as e:
+            raise errors.AnsibleFilterError(
+                    'each_format plugin error: {0}, string_format={1}'.format(str(e), str(string_format))
+            )
 
         
